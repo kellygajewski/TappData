7d89d92a502c965a022b7ee212dbb99acbb1f949
Kelly Gajewski <kellyagajewski@gmail.com>
2014-07-01 18:56:47 -0700

    cleans up code slightly

--- app/controllers/beerdata_controller.rb
+++ app/controllers/beerdata_controller.rb
@@ -4,20 +4,7 @@ class BeerdataController < ApplicationController
   	# before_action :get_feed, only: [:feed]
 
   	def index  	
-  # 		@month_counts=[]
-		# @jan_count = []
-		# @feb_count = []
-		# @mar_count = []
-		# @apr_count = []
-		# @may_count = []
-		# @jun_count = []
-		# @jul_count = []
-		# @aug_count = []
-		# @sep_count = []
-		# @oct_count = []
-		# @nov_count = []
-		# @dec_count = []
-  # 		main_style(@response)
+
   	end
 
   	def feed
@@ -47,24 +34,6 @@ class BeerdataController < ApplicationController
 	  			@response = Untappd::User.feed(@username, {:limit => 50, :max_id => last_id_num}).checkins.items
 	  		end
   	end
-
-	#Should return the top 3 nouns, noun phrases, or adjectives per description sample using the Engtagger gem
-	def self.top_words
-		tgr = EngTagger.new
-		# self.each do |b|
-		# 	desc_array.push(b.beer_description)
-		# end
-		# desc_string = desc_array.join(" ")
-		desc_string = "hops hops hops coffee coffee beer yum yum yum"
-		tagged = tgr.add_tags(desc_string)
-		nouns = tgr.get_words(tagged)
-		adj = tgr.get_adjectives(tagged)
-		words = nouns.merge(adj)
-		words.sort {|k,v| k[-1]<=>v[-1]}
-		return "#{words[0][0]}, #{words[1][0]}, #{words[2][0]}"
-	end
-
-	
 
 
 
@@ -118,9 +87,6 @@ class BeerdataController < ApplicationController
 
 
 
-	
-
-
 	def month_finder(response, category)
 
 		month = response.created_at[8..10]
@@ -168,10 +134,11 @@ class BeerdataController < ApplicationController
 	end
 
 	
-#Creates 2D array of the number of checkins per each month (rows/layers) per beer style (columns/bars). Next step is to convert each element into an object where each x is the column index (relates to each style) and each y is the number of checkins for that beer style per month ie {x: 0, y: 23} and this array can then be fed into the D3 stacked bar chart.
+#Creates 2D array of the number of checkins per each month (rows/layers) per beer style (columns/bars). 
 	def calc_beer_months(x)
 		@top_beer_styles=["Porter", "Stout", "Brown Ale", "Pale Ale", "Wheat Beer", "Other Ale", "Pilsner", "American Light Lager", "Other Lager", "Cider", "Other"]
 
+		#creates a 12 x 11 array of 0s
 		@beer_months = Array.new(12) { Array.new(11, 0)}
 
 		#for each of the arrays in month_counts corresponding to each month
@@ -183,12 +150,12 @@ class BeerdataController < ApplicationController
 	        	#if the beer style of the checkin equals the beer style in the array, increment the value of number of beers for that style for that month
 	          if (that_months_beer == one_beer_style)  
 	            @beer_months[@month_counts.index(x)][@top_beer_styles.index(one_beer_style)]+=1
-	            $beertestz = 1
 	          end	     
 	        end
 	      end
 		end
 
+#Last minute fix to get the data in the proper format for D3. Each element is converted into an object where each x is the column index (relates to each style) and each y is the number of checkins for that beer style per month ie {x: 0, y: 23} and this array can then be fed into the D3 stacked bar chart. This could likely be handled better from an object-oriented standpoint.
 		@beer_months_object = []
 		@beer_months.each do |m|
 			this_month = []
@@ -200,31 +167,24 @@ class BeerdataController < ApplicationController
 	end
 	
 
-#Should return the broader category for a beer given its beer_style
-	# def main_style
-	# 	if self.beer_style.include?("Porter")
-	# 		return "Porter"
-	# 	elsif self.beer_style.include?("Stout")
-	# 		return "Stout"
-	# 	elsif self.beer_style.include?("Brown Ale") || self.beer_style.include?("Dubbel")
-	# 		return "Brown Ale" 
-	# 	elsif ((self.beer_style.include?("Pale") || self.beer_style.include?("Amber") || self.beer_style.include?("Red") ||  self.beer_style.include?("Blonde") || self.beer_style.include?("Scottish")) && self.beer_style.include?("Ale")) || self.beer_style.include?("Belgian Blonde") || self.beer_style.include?("IPA") || self.beer_style.include?("Barleywine") || self.beer_style.include?("Bière de Garde") || self.beer_style.include?("Saison") || self.beer_style.include?("Bitter")
-	# 		return "Pale Ale" 
-	# 	elsif self.beer_style.include?("Hefeweizen") || self.beer_style.include?("Witbier")
-	# 		return "Wheat Beer"
-	# 	elsif self.beer_style.include?("Ale") || self.beer_style.include?("Altbier") || self.beer_style.include?("Quadrupel")
-	# 		return "Other Ale"
-	# 	elsif self.beer_style.include?("American Adjunct Lager") || self.beer_style.include?("American Light Lager")
-	# 		return "American Lager"
-	# 	elsif self.beer_style.include?("Lager") || self.beer_style.include?("bock") || self.beer_style.include?("Bock")
-	# 		return "Other Lager"
-	# 	elsif self.beer_style.include?("Pilsner")
-	# 		return "Pilsner"
-	# 	elsif self.beer_style.include?("Cider")
-	# 		return "Cider"
-	# 	else
-	# 		return "Other"
-	# 	end	
+
+#Should return the top 3 nouns, noun phrases, or adjectives per description sample using the Engtagger gem. Still in progress.
+	def self.top_words
+		tgr = EngTagger.new
+		# self.each do |b|
+		# 	desc_array.push(b.beer_description)
+		# end
+		# desc_string = desc_array.join(" ")
+
+		tagged = tgr.add_tags(desc_string)
+		nouns = tgr.get_words(tagged)
+		adj = tgr.get_adjectives(tagged)
+		words = nouns.merge(adj)
+		words.sort {|k,v| k[-1]<=>v[-1]}
+		return "#{words[0][0]}, #{words[1][0]}, #{words[2][0]}"
+	end
+
+
 	private
 
 	def user_params
